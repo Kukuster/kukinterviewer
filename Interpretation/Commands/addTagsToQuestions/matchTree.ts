e182ec5a81382c324816daed4c9980e2ba135f06
Kukuster <KukusterMOP@gmail.com>
2020-08-07 19:17:22 +0300

    added 'matchTree', 'match', 'prepare', and the appropriate testCases and tests for the following Commands: addTagsToQuestions, askMeAQuestion, delete Questions

--- Interpretation/Commands/addTagsToQuestions/matchTree.ts
+++ Interpretation/Commands/addTagsToQuestions/matchTree.ts
@@ -4,31 +4,135 @@ import { node, nodeLike, SELF, PARENTs_CHILDREN } from "../../matchTree/node";
 // any punctuation mark at the end: 
 // [\?\!\.,;:]*$
 // (?:\?|\!|\.|,|;|:|$)+
-const rootRE = /[\s\S]+/g;
-const turnRE = /^turn(ing|ed)?[\?\!\.,;:]*$/i
-const enableRE  =  /^enabl(e|ing|ed)[\?\!\.,;:]*$/i;
-const disableRE = /^disabl(e|ing|ed)[\?\!\.,;:]*$/i;
-const onRE  =  /^on[\?\!\.,;:]*$/i;
-const offRE = /^off[\?\!\.,;:]*$/i;
-const questionRE = /^questions?(\S*)*/i;
-const allRE = /^(all|each|every)[\?\!\.,;:]*$/i;
-const digitRE = /(\d+)/g;
+const root = /[\s\S]+/g;
+
+const add = /^(add(ing|ed)?|new|creat(e|ing|ed)|insert(ing|ed)?|submit(ing|ed)?|includ(e|ing|ed))[\?\!\.,;:]*$/i;
+const question = /^questions?(\S*)*/i;
+const questionDigit = /^questions?[\?\!\.,;:]*(#|№|N|n|@)(\d+)[\?\!\.,;:]*/i;
+const tag = /#([0-9_]*([a-zA-Z]+[0-9_]*)+)/g;
+const digit = /(#|№|@|n(um(ber)?)?)?(\d+)(st|nd|rd|th)?[\?\!\.,;:]*/gi;
+const all = /^(all|each|every)[\?\!\.,;:]*$/i;
+const tagWord = /^(hash)?tag(ged|ging)?[\?\!\.,;:]*$/i;
+
+const get = /^(get(ting)?|mak(e|ing)|let(ting)?|got|made)[\?\!\.,;:]*$/i;
+
+
+const frb = /^(eras(e|ing)|remov(e|ing)|turn(ing)?|delet(e|ing)|eliminat(e|ing)|destro(y|ing)|drop(ping)?|wip(e|ing)|withdraw(ing)?|enabl(e|ing)|disabl(e|ing)|dismiss(ing)?)[\?\!\.,;:]*$/i;
+
+const neg = /^(don't|never|not?)*[\?\!\.,;:]*$/i;
 
 
 
-export type shoot = {  };
+export type shoot   = { questions: 'all'|'some' };
 
-const aShoot  :shoot = {   };
-
-
-let OnDigitRecursiveShoot: nodeLike,
-    OffDigitRecursiveShoot: nodeLike,
-    turnOnBranch: nodeLike[],
-    turnOffBranch: nodeLike[];
+const qSome  :shoot = { questions: 'some' };
+const qAll   :shoot = { questions: 'all' }
 
 
-export const addTagsToQuestion_tree =
-    node(rootRE, [
+let Branch: nodeLike[];
 
 
-    ]);
+export const addTagsToQuestions_tree =
+node(root, [
+
+    node(add, [
+        node(tag, [
+            SELF,
+            node(question, [
+                node(digit, [SELF], qSome as shoot),
+            ]),
+
+            node(all, [
+                node(question, [], qAll as shoot),
+            ]),
+
+            // add -> # -> ...
+            node(neg, []),
+            node(frb, []),
+        ]),
+
+        node(question, [
+            node(digit, [
+                SELF,
+                node(tag, [SELF], qSome as shoot),
+            ]),
+
+            // add -> question -> ...
+            node(neg, []),
+            node(frb, []),
+        ]),
+
+        // add -> ...
+        node(neg, []),
+        node(frb, []),
+
+    ]), // add
+
+
+    node(tagWord, [
+        node(question, [
+            node(digit, [
+                node(tag, [SELF], qSome as shoot),
+                SELF
+            ]),
+
+            // tagWord -> question -> ...
+            node(neg, []),
+            node(frb, []),
+        ]),
+
+
+        node(tag, [
+            SELF,
+            node(all, [
+                node(question, [], qAll as shoot)
+            ]),
+
+            // tagWord -> # -> ...
+            node(neg, []),
+            node(frb, []),
+        ]),
+
+        node(digit, [
+            node(question, [
+                node(tag, [SELF], qSome as shoot)
+            ]),
+            SELF,
+
+            // tagWord -> digit -> ...
+            node(neg, []),
+            node(frb, []),
+        ]),
+
+
+        // tagWord -> ...
+        node(neg, []),
+        node(frb, []),
+
+    ]), // tagWord
+
+
+    node(get, [
+        node(question, [
+            node(digit, [
+                SELF,
+                node(tagWord, [
+                    node(tag, [SELF], qSome as shoot),
+                ]),
+            ]),
+
+            // tagWord -> question -> digit -> ...
+            node(neg, []),
+            node(frb, []),
+        ]),
+        // tagWord -> question -> ...
+        node(neg, []),
+        node(frb, []),
+    ]),
+
+
+    node(neg, []),
+    node(frb, []),
+
+
+]);
--- Interpretation/Commands/deleteQuestions/matchTree.ts
+++ Interpretation/Commands/deleteQuestions/matchTree.ts
@@ -1,34 +1,314 @@
 import { node, nodeLike, SELF, PARENTs_CHILDREN } from "../../matchTree/node";
+import { questionsQuery } from "../../../core/sheet/methods/questions/getQuestions";
 
 
 // any punctuation mark at the end: 
 // [\?\!\.,;:]*$
 // (?:\?|\!|\.|,|;|:|$)+
-const rootRE = /[\s\S]+/g;
-const turnRE = /^turn(ing|ed)?[\?\!\.,;:]*$/i
-const enableRE  =  /^enabl(e|ing|ed)[\?\!\.,;:]*$/i;
-const disableRE = /^disabl(e|ing|ed)[\?\!\.,;:]*$/i;
-const onRE  =  /^on[\?\!\.,;:]*$/i;
-const offRE = /^off[\?\!\.,;:]*$/i;
+const root = /[\s\S]+/g;
 const questionRE = /^questions?(\S*)*/i;
 const allRE = /^(all|each|every)[\?\!\.,;:]*$/i;
 const digitRE = /(\d+)/g;
 
 
+const del = /^(delet(e|in(g|')?)?|eras(e|in(g|')?)?|remov(e|in(g|')?)?|eliminat(e|in(g|')?)?|drop(pin('|g)?)?|wip(e|in(g|')?)?)*[\?\!\.,;:]*$/i;
+const question = /^questions?[\?\!\.,;:]*$/i;
+const questions =/^questions[\?\!\.,;:]*$/i;
+const questionDigit = /^questions?[\?\!\.,;:]*(#|№|N|n|@)(\d+)[\?\!\.,;:]*/i;
+const tag = /#([0-9_]*([a-zA-Z]+[0-9_]*)+)/g;
+const digit = /(#|№|@|n(um(ber)?)?)?(\d+)(st|nd|rd|th)?[\?\!\.,;:]*/gi;
+const all = /^(all|each|every)[\?\!\.,;:]*$/i;
+const tagWord = /^(hash)?tag(ged|ging|s)?[\?\!\.,;:]*$/i;
+const untagWord = /^un(hash)?tag(ged|ging)?[\?\!\.,;:]*$/i;
 
-export type shoot = {  };
+const turn = /^turn(ing|ed)?[\?\!\.,;:]*$/i
+const on  =  /^on[\?\!\.,;:]*$/i;
+const off = /^off[\?\!\.,;:]*$/i;
 
-const aShoot  :shoot = {   };
+const enable  =  /^enabl(e|ing|ed)[\?\!\.,;:]*$/i;
+const disable = /^disabl(e|ing|ed)[\?\!\.,;:]*$/i;
+
+//const add = /^(add(ing|ed)?|new|creat(e|ing|ed)|insert(ing|ed)?|submit(ing|ed)?|includ(e|ing|ed))[\?\!\.,;:]*$/i;
 
 
-let OnDigitRecursiveShoot: nodeLike,
-    OffDigitRecursiveShoot: nodeLike,
-    turnOnBranch: nodeLike[],
-    turnOffBranch: nodeLike[];
+const frb = /^(search|turn(in('|g)?)?|withdraw(in('|g)?)?|enabl(e|in('|g)?)|disabl(e|in('|g)?)|dismiss(in('|g)?)?|add(in('|g)?)?|new|creat(e|in('|g)?)|insert(in('|g)?)?|submit(in('|g)?)?|includ(e|in('|g)?)|ask(ed|in('|g)?)?)[\?\!\.,;:]*$/i;
+
+const neg  = /^(not?|without|don'?t|haven'?t|hadn'?t|weren'?t|aren'?t|ain'?t)[\?\!\.,;:]*$/i;
+const dont = /^(don't|never)[\?\!\.,;:]*$/i;
+
+
+export type shoot = {
+    qids?: 'some',
+    enabled?: boolean,
+    Tags?: 'some' | 'no' | 'any',
+} | 'all';
+
+const allShoot:         shoot = 'all';
+const taggedWithShoot:  shoot = { Tags: 'some' }
+const untaggedShoot:    shoot = { Tags: 'no' };
+const taggedShoot:   shoot = { Tags: 'any' };
+const enabledShoot:     shoot = { enabled: true };
+const disabledShoot:    shoot = { enabled: false };
+const qidsShoot:        shoot = { qids: 'some' };
+
+
+
+let delQuestionsNot:     nodeLike[],
+    delQuestions:        nodeLike[],
+    delAllSmthQuestions: nodeLike[];
 
 
 export const deleteQuestions_tree =
-    node(rootRE, [
+node(root, [
+
+    node(del, [
+
+        node(all, [
+            
+            // delete -> all -> ...
+            node(question, delQuestions =  [
+
+                // delete -> all -> questions -> ...
+                node(tag, [
+                    node(frb, [])
+                ], taggedWithShoot as shoot),
+
+                node(tagWord, [
+                    node(tag, [
+                        node(frb, [])
+                    ], taggedWithShoot as shoot),
+                    node(frb, [])    
+                ], taggedShoot as shoot),
+
+                node(untagWord, [
+                    node(frb, [])
+                ], untaggedShoot as shoot),
 
 
-    ]);
+                // delete -> all -> questions -> ...
+                node(neg, delQuestionsNot = [
+                    node(tagWord, [
+                        node(frb, [])
+                    ], untaggedShoot as shoot),
+
+                    node(untagWord, [
+                        node(frb, [])
+                    ], taggedShoot as shoot),
+
+                    // delete -> all -> questions -> no[t] -> ...
+                    node(turn, [
+                        node(on, [
+                            node(frb, [])
+                        ], disabledShoot as shoot),
+                        node(off, [
+                            node(frb, [])
+                        ], enabledShoot as shoot),
+
+                        node(frb, [])
+                    ]),
+
+                    // delete -> all -> questions -> no[t] -> ...
+                    node(enable, [
+                        node(frb, [])
+                    ], disabledShoot as shoot),
+
+                    // delete -> all -> questions -> no[t] -> ...
+                    node(disable, [
+                        node(frb, [])
+                    ], enabledShoot as shoot),
+
+                    node(frb, []),
+                ]),
+
+
+                // delete -> all -> questions -> ...
+                node(turn, [
+                    node(on, [
+                        node(frb, [])
+                    ], enabledShoot as shoot),
+                    node(off, [
+                        node(frb, [])
+                    ], disabledShoot as shoot),
+                    node(frb, [])
+                ]),
+
+
+                // delete -> all -> questions -> ...
+                node(enable, [
+                    node(frb, [])
+                ], enabledShoot as shoot),
+                node(disable, [
+                    node(frb, [])
+                ], disabledShoot as shoot),
+
+
+                node(frb, [])
+            ], allShoot as shoot), // delete -> all -> questions
+
+            ...( delAllSmthQuestions = [
+            // delete -> all -> ...
+            node(tag, [
+                node(questions, [
+                    node(frb, [])
+                ], taggedWithShoot as shoot),
+                node(frb, [])
+            ]),
+
+            // delete -> all -> ...
+            node(tagWord, [
+                node(tag, [
+                    SELF,
+                    node(questions, [
+                        node(frb, [])
+                    ], taggedWithShoot as shoot),
+                    node(frb, [])
+                ]),
+                node(questions, [
+                    node(frb, [])
+                ], taggedShoot as shoot),
+                node(frb, [])
+            ]),
+
+            // delete -> all -> ...
+            node(untagWord, [
+                node(questions, [
+                    node(frb, [])
+                ], untaggedShoot as shoot),
+                node(frb, [])
+            ]),
+
+            // delete -> all -> ...
+            node(turn, [
+                node(on, [
+                    node(questions, [
+                        node(frb, [])
+                    ], enabledShoot as shoot),
+                    node(frb, [])
+                ]),
+                node(off, [
+                    node(questions, [
+                        node(frb, [])
+                    ], disabledShoot as shoot),
+                    node(frb, [])
+                ]),
+                node(tagWord, []),
+                node(frb, [])
+            ]),
+
+            // delete -> all -> ...
+            node(enable, [
+                node(questions, [
+                    node(frb, [])
+                ], enabledShoot as shoot),
+                node(tagWord, []),
+                node(frb, [])
+            ]),
+
+            // delete -> all -> ...
+            node(disable, [
+                node(questions, [
+                    node(frb, [])
+                ], disabledShoot as shoot),
+                node(tagWord, []),
+                node(frb, [])
+            ]),
+
+            
+            // delete -> all -> ...
+            node(neg, [
+
+                // delete -> all -> no[t] -> ...
+                node(tagWord, [
+                    node(questions, [
+                        node(frb, [])
+                    ], untaggedShoot as shoot),
+                    node(frb, [])
+                ]),
+
+                node(untagWord, [
+                    node(questions, [
+                        node(frb, [])
+                    ], taggedShoot as shoot),
+                    node(frb, [])
+                ]),
+
+                // delete -> all -> no[t] -> ...
+                node(turn, [
+                    node(on, [
+                        node(questions, [
+                            node(frb, [])
+                        ], disabledShoot as shoot),
+                        node(frb, [])
+                    ]),
+                    node(off, [
+                        node(questions, [
+                            node(frb, [])
+                        ], enabledShoot as shoot),
+                        node(frb, [])
+                    ]),
+
+                    node(frb, [])
+                ]),
+
+                // delete -> all -> no[t] -> ...
+                node(enable, [
+                    node(questions, [
+                        node(frb, [])
+                    ], disabledShoot as shoot),
+                    node(frb, [])
+                ]),
+
+                // delete -> all -> no[t] -> ...
+                node(disable, [
+                    node(questions, [
+                        node(frb, [])
+                    ], enabledShoot as shoot),
+                    node(frb, [])
+                ]),
+
+                node(frb, [])
+            ]), // delete -> all -> no[t]
+
+            ]), // ...( delAllSmthQuestions = [...] )
+
+
+            node(frb, [])
+        ]), // delete -> all
+
+        // delete -> ...
+        node(question, [
+            ...delQuestions,
+            node(digit, [
+                SELF,
+                node(frb, [])
+            ], qidsShoot as shoot),
+        ]), // delete -> question
+
+        // delete -> ...
+        ...delAllSmthQuestions,
+
+        // delete -> ...
+        node(digit, [
+            SELF,
+            node(question, [
+                node(frb, [])
+            ], qidsShoot),
+            node(frb, [])
+        ]),     
+
+        node(frb, [])
+    ]), // delete
+
+
+    node(dont, []),
+    node(frb, [])
+
+]);
+
+
+// node(RE, [
+
+//     node(frb, [])
+// ]),
+
+    

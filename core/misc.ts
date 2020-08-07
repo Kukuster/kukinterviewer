cfe144d7164d31e1de9585f23726d3912d9840bf
Kukuster <KukusterMOP@gmail.com>
2020-08-07 19:17:22 +0300

    added functions to trim string up to substring from beginning and one for from the end

--- core/misc.ts
+++ core/misc.ts
@@ -14,3 +14,22 @@ export function uniquifyArray(ar: any[]) {
         return j[v];
     });
 } 
+
+/**
+ * Cuts the given _string_ from the beginning up to the first occurance of a given _substring_ inclusively, ignoring case of the _substring_
+ * @param s a _string_ to cut
+ * @param ss a _substring_ to search for the first occurance and cut up to (inclusively)
+ */
+export const cutOffUpToWithFirstOccurance = (s: string, ss: string) => s.replace(new RegExp('^(.|\r|\n)*?' + ss, 'i'), '');
+// (.|\r|\n)* matches anything of any length
+// ? at the end means non-greedy
+
+
+/**
+ * Cuts the given _string_ from the first occurance of a given _substring_ inclusively up to the end, ignoring case of the _substring_
+ * @param s a _string_ to cut
+ * @param ss a _substring_ to search for the first occurance and cut from (inclusively)
+ */
+export const cutOffFromWithFirstOccurance = (s: string, ss: string) => s.replace(new RegExp(ss + '(.|\r|\n)*?$', 'i'), '');
+// (.|\r|\n)* matches anything of any length
+// ? at the end means non-greedy

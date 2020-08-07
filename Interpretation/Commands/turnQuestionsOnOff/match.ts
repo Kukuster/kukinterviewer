e0bf27c31cf407d2805077b9894288798f72d39b
Kukuster <KukusterMOP@gmail.com>
2020-08-07 19:17:19 +0300

    small refactoring and comments on matchTree functionality. Decided that matchTree testCases should be in the directory with the command

--- Interpretation/Commands/turnQuestionsOnOff/match.ts
+++ Interpretation/Commands/turnQuestionsOnOff/match.ts
@@ -1,6 +1,6 @@
 import { IIMessage } from "../../../core/Command/Command";
 import { splitToWords } from "../../matchTree/splitToWords";
-import { walk, nodeStep } from "../../matchTree/walk";
+import { traverse, nodeStep } from "../../matchTree/walk";
 import { turnQuestionsOnOff_tree } from "./matchTree";
 
 
@@ -18,7 +18,7 @@ export default async function turnQuestionsOnOff_match(msg: IIMessage): Promise<
 
     const path: nodeStep[] = [];
     
-    walk(turnQuestionsOnOff_tree, Words, path);
+    traverse(turnQuestionsOnOff_tree, Words, path);
     
     return (path.length && path[path.length - 1].shoot) ? 
                 path :

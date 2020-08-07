4bf1817c62538d7249e02538e4a720510f41b6e1
Kukuster <KukusterMOP@gmail.com>
2020-08-07 19:17:21 +0300

    removed States mock from index.ts

---
+++ Interpretation/States/greet.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+import State from "../../core/State/State";
+
+export default new State('greet', []);
+
--- index.ts
+++ index.ts
@@ -3,17 +3,10 @@
 import bot from "./bot";
 import { decide } from "./Interpretation/decide";
 import State from "./core/State/State";
+import States from "./Interpretation/States"
+import greetState from "./Interpretation/States/greet";
 
 bot.on("polling_error", (err) => console.log(err));
-
-/**
- * temp mock
- */
-const States: State[] = [];
-/**
- * temp mock
- */
-const greetState = new State('greet', []);
 
 
 bot.on('message', (msg) => {

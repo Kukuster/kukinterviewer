ae92dc68a295b2481214013586f6759acd927dae
Kukuster <KukusterMOP@gmail.com>
2020-10-07 16:07:52 +0300

    finished matchTrees, matchTree tests, match, prepare for Commands listTags & turnTagsOnOff

--- Interpretation/Commands/listTags/prepare.ts
+++ Interpretation/Commands/listTags/prepare.ts
@@ -2,14 +2,16 @@ import { IIMessage } from "../../../core/Command/Command";
 import { treeStep } from "../../matchTree/walk";
 import { shoot } from "./matchTree";
 import { uniquifyArray } from "../../../core/misc";
+import { tagsQuery } from "../../../core/sheet/methods/tags/getTags";
+import { passedTree_to_TagsQuery } from "../../matchTree/extras/passedTree_to_TagsQuery";
 
 
-export default async function listTags_prepare (msg: IIMessage, path: treeStep[]): Promise<{  }> {
+export default async function listTags_prepare(msg: IIMessage, path: treeStep[]): Promise<tagsQuery | 'all'> {
 
 
     const theShoot: shoot = path[path.length - 1].shoot;
 
-    return {};
+    return passedTree_to_TagsQuery(path);
     
 
 }
--- Interpretation/Commands/turnTagsOnOff/matchTree_testCases.ts
+++ Interpretation/Commands/turnTagsOnOff/matchTree_testCases.ts
@@ -1,87 +1,91 @@
 export type turnTagsOnOff_testCase = 
-    { m: string,                                                res: { Tags: string[] | 'all', status: 'on' | 'off' } | null };
+    { m: string,                                                res: { Tags: string[] | 'all', turn: 'on' | 'off' } | null };
 export const turnTagsOnOff_testCases: turnTagsOnOff_testCase[] = [
 
 
     // turn -> # -> on/off
     { m: "turn #tag1 tag on ",                                                             res: { 
         Tags: ['tag1'],
-        status: 'on'
+        turn: 'on'
      } },
     { m: "turn tags #tag1 #tag2 #web off ",                                                res: { 
         Tags: ['tag1','tag2','web'],
-        status: 'off'
+        turn: 'off'
      } },
     { m: "would you mind turning #web off? ",                                              res: { 
         Tags: ['web'],
-        status: 'off'
+        turn: 'off'
      } },
-    { m: "hey, maybe turn #web off? ",                                                     res: {  
+    //{ m: "hey, maybe turn #web off? ",                                                     res: {  
+    { m: "maybe turn #web off? ",                                                          res: {    
         Tags: ['web'],
-        status: 'off'
-    } },
-    { m: "can I please have tags #web #css turned off? ",                                  res: { 
-        Tags: ['web','css'],
-        status: 'off'
+        turn: 'off'
      } },
     { m: "turn #web off! ",                                                                res: { 
         Tags: ['web'],
-        status: 'off'
+        turn: 'off'
      } },
 
 
     // turn -> on/off -> #
     { m: "turn off #tag tag ",                                                             res: { 
         Tags: ['tag'],
-        status: 'off'
+        turn: 'off'
      } },
     { m: "turn off #tag1 #tag2 #tag3 tags ",                                               res: { 
-        Tags: ['tag1','tag2','tag2','tag3'],
-        status: 'off'
+        Tags: ['tag1','tag2','tag3'],
+        turn: 'off'
+     } },
+
+    
+    // # -> turn -> on/off
+    { m: "can I please have tags #web #css turned off? ",                                  res: { 
+        Tags: ['web','css'],
+        turn: 'off'
      } },
 
 
     // enable/disable -> #
     { m: "enable tag #tag1 ",                                                              res: { 
         Tags: ['tag1'],
-        status: 'on'
+        turn: 'on'
      } },
     { m: "disable #web ",                                                                  res: { 
         Tags: ['web'],
-        status: 'off'
+        turn: 'off'
      } },
     { m: "enable #tag1 #tag2 ",                                                            res: { 
         Tags: ['tag1','tag2'],
-        status: 'on'
+        turn: 'on'
      } },
     { m: "how about disabling #web tag? ",                                                 res: { 
         Tags: ['web'],
-        status: 'off'
+        turn: 'off'
      } },
     { m: "don't mind enabling #web ?! ",                                                   res: { 
         Tags: ['web'],
-        status: 'on'
+        turn: 'on'
      } },
 
 
     // turn -> on/off -> all -> tags
     { m: "turn on all the tags ",                                                          res: { 
         Tags: 'all',
-        status: 'on'
+        turn: 'on'
      } },
 
 
     // turn -> all -> tags -> on/off
     { m: "turn all the tags off ",                                                         res: { 
         Tags: 'all',
-        status: 'off'
+        turn: 'off'
      } },
 
 
     // enable/disable -> all -> tags
     { m: "enable all tags ",                                                               res: { 
         Tags: 'all',
-        status: 'on'
+        turn: 'on'
      } },
      
 
@@ -91,7 +95,7 @@ export const turnTagsOnOff_testCases: turnTagsOnOff_testCase[] = [
     { m: "turn on the tag ",                                                               res: null },
     { m: "turnage all the tags ",                                                          res: null },
     { m: "turn #tag1 #tag2 #web tags ",                                                    res: null },
-    { m: "tags #tag1 #tag2 turn off ",                                                     res: null },
+    //{ m: "tags #tag1 #tag2 turn off ",                                                     res: null },
     { m: "enable tags ",                                                                   res: null },
     { m: "turn tags ",                                                                     res: null },
     { m: "turn all ",                                                                      res: null },
@@ -102,7 +106,7 @@ export const turnTagsOnOff_testCases: turnTagsOnOff_testCase[] = [
 
     // [ forbidden ]
     { m: "turn off #tag1 #tag2 #tag3 questions tags ",                                     res: null },
-    { m: "disable on tags #tag1 #tag2 ",                                                   res: null },
+    //{ m: "disable on tags #tag1 #tag2 ",                                                   res: null },
     { m: "turn off and on all the tags ",                                                  res: null },
     { m: "turn all the questions with #tag1 #tag2 #web off ",                              res: null },
     { m: "turning all the questions on ",                                                  res: null },

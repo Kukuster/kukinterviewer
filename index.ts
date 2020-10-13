94ef1d95382f542f43eee7c4a4c71c483d780270
Kukuster <KukusterMOP@gmail.com>
2020-10-13 18:16:26 +0300

    included mongoose connection. Now the bot actually works (3 Commands: addQuestion, listQuestions, deleteQuestions) with the default display method

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 'use strict';
 
+// https://github.com/yagop/node-telegram-bot-api/issues/319
+process.env.NTBA_FIX_319 = '1';
+
 import bot from "./bot";
 import { decide } from "./Interpretation/decide";
 import State from "./core/State/State";
 import States from "./Interpretation/States"
 import greetState from "./Interpretation/States/greet";
+import mongoose from "./core/sheet/mongoose";
+
+
+mongoose.dbPromise;
 
 bot.on("polling_error", (err) => console.log(err));
 
@@ -14,6 +21,7 @@ bot.on('message', (msg) => {
 
     console.log('recieved message:');
     console.log(msg);
+    console.log();
 
     decide(msg, States, greetState);
 

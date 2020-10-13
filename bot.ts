82872a3b7a47bc79a4e3a06717ca167bd2f5f4df
Kukuster <KukusterMOP@gmail.com>
2020-10-13 18:09:14 +0300

    moved `monospace` function to bot.ts

--- bot.ts
+++ bot.ts
@@ -6,8 +6,18 @@ import { TOKEN } from './conf';
 
 if (!TOKEN) {
     process.exit(ec.noTelegramBotApiTOKEN);
-}
+};
 
 export default new TelegramBot(TOKEN, { polling: true });
+
+/**
+ * Telegram formating: monospace text
+ * Requires `parse_mode: 'Markdown'` option to be added to options object (3rd argument for bot.sendMessage method)
+ * 
+ * added endlines affect message design, but it doen't work on all devices without them
+ */
+export function monospace(str: string): string {
+    return '```\n' + str + '\n```';
+};
 
 

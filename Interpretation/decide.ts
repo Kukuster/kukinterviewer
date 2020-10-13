1c72f7cdc53c85e5220c030131546d4a04e41b2a
Kukuster <KukusterMOP@gmail.com>
2020-10-13 15:16:16 +0300

    updated decide function to use latest sheet methods, added some comments to it

--- Interpretation/decide.ts
+++ Interpretation/decide.ts
@@ -1,46 +1,60 @@
 'use strict';
 import { IIMessage } from "../core/Command/Command";
 import State from "../core/State/State";
-import ChatModel from "../core/sheet/models/ChatModel";
+import getChat from "../core/sheet/methods/chat/getChat";
 
 
+/**
+ * 
+ * This function is used by the bot to decide what Command to execute depending on the recieved message and the current State
+ * 
+ * @param message recieved Telegram message
+ * @param States an array of States supported by the app. Each State contains a string identifier and an array of Commands to check for
+ * @param defaultState a State that is assumed by default if a user has no chat document in the DB (thus doesn't have state)
+ * 
+ * @returns Promise<void> if no Command was executed, Promise for a returned value from a `{Command}.display` method otherwise.
+ * 
+ */
 export async function decide(message: IIMessage, States: State[], defaultState: State){
     const chatId = message.chat.id;
-    
-    const result = await ChatModel.find({ chatId: chatId }).select({ "state": true, "_id": false }).exec();
+
+    const DB_chat = await getChat(chatId, { "state": true, "_id": false });
 
     let theState: State | undefined;
     
 
-    if (!result.length){ 
+    if (!DB_chat){
         theState = defaultState;
-
     } else {
-        if (!result[0].state){
+
+        if (!DB_chat.state) {
             console.error(new Error('ERROR while getting chat state. Got chat document object, but it has no *state* property'));
             return;
-        }
+        };
 
-        theState = States.find(s => s.name === result[0].state);
+        theState = States.find(s => s.name === DB_chat.state);
 
-        if (!theState){
+        if (!theState) {
             theState = defaultState;
-        }
+        };
 
-    }
-
-    //given the state, deciding on which command the message matches
-
+    };
+   
+    /**
+     * Given the state and its array of Commands that the bot may expect,
+     * decices which Command the message matches (by executing a Command's match method against the message)
+     * if a match is found, proceed on doing .prepare, .execute, and .display for such Command
+     * This is how the bot decides on the Command
+    */
     for (const i in theState.Commands){
         let match;
         if ( match     = await theState.Commands[i].match  (message)){
             const args = await theState.Commands[i].prepare(message, match);
             const resp = await theState.Commands[i].execute(message, args);
                   return await theState.Commands[i].display(message, resp);
-        }
+        };
 
-    }
+    };
 
 
-}
-
+};

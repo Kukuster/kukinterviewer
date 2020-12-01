404785bf6ed9eea27de30e6281b91a1c7ab7fa4e
Kukuster <KukusterMOP@gmail.com>
2020-12-01 20:14:22 +0200

    core/sheet/methods/chat/getAllChats.ts: added a chat method for querying all chats

---
+++ src/core/sheet/methods/chat/getAllChats.ts
@@ -0,0 +1,13 @@
+'use strict';
+import ChatModel, { Ichat, Ichat_select } from "../../models/ChatModel";
+
+
+/**
+ * 
+ * @param select Specifies which document fields to include or exclude (also known as the query "projection")
+ * @returns result of querying Chat documents
+ * 
+ */
+export default async function getAllChats(select?: Ichat_select): Promise<Ichat[] | null> {
+    return ChatModel.find().select(select).exec();
+}

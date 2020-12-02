b550804711bb7bb99d1e7de62027a50086222802
Kukuster <KukusterMOP@gmail.com>
2020-12-02 17:15:46 +0200

    build-frontend.sh: works with new frontend now

--- build-frontend.sh
+++ build-frontend.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-cd src/assets && \
-npm run build && \
-cd - && \
-rm -rf dist/assets && \
-mv src/assets/build dist/assets
+cd src/web/frontend && \
+npm run build_andExit && \
+cd - 1>/dev/null && \
+rm -rf dist/web/frontend && \
+mkdir -p dist/web/frontend 1>/dev/null 2>&1
+mv src/web/frontend/dist dist/web/frontend

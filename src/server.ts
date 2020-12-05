e4c24a7b53ba30ffc47d322d247d4e0922250390
Kukuster <KukusterMOP@gmail.com>
2020-12-05 22:37:31 +0200

    made an express server to serve a few frontend pages and some api

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { PORT, HOST } from './conf';
 import express from 'express';
 import { inspect } from 'util';
 import getChat from './core/sheet/methods/chat/getChat';
+import getAllChats from './core/sheet/methods/chat/getAllChats';
 
 
 console.log('hello from server.js');
@@ -26,120 +27,97 @@ function safeStringify<T> (obj: T, indent = 2) {
 
 const server = express();
 
+server.use(express.static('web/frontend'));
+
+
 server.get('', (req, res) => {
-    fs.readFile('./index.html', function(error, content){
-        error ? 
+    console.log("server.get('')");
+
+    const home = '/web/frontend/dist/html/home.html';
+    const home_full = __dirname + home;
+    fs.readFile(home_full, (error, content) => {
+        if (error) {
             res.writeHead(500) &&
-            res.end('HTTP 500')
-        :
+                res.end('HTTP 500');
+            console.log('error reading file "' + home_full + '": ', { error });
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' }) &&
+                res.end(content, 'utf-8');
+            console.log('successfully server contents of a requested file ' + home_full + '');
+        }
+    });
+    // res.redirect('/app');
+
+    console.log('server.ts: ',{__dirname});
+});
+
+
+server.get('/app', (req, res) => {
+    console.log("server.get('/app')");
+
+    const frontendApp = '/web/frontend/dist/html/app.html';
+    const frontendApp_full = __dirname + frontendApp;
+    fs.readFile(frontendApp_full, (error, content) => {
+        if (error) {
+            res.writeHead(500) &&
+            res.end('HTTP 500');
+            console.log('error reading file "'+frontendApp_full+'": ', { error });
+        } else {
             res.writeHead(200, { 'Content-Type': 'text/html' }) &&
             res.end(content, 'utf-8');
+            console.log('successfully server contents of a requested file '+frontendApp_full+'');
+        }
     });
+    console.log('server.ts: ',{__dirname});
 });
+
+
 
 server.get('/api', (req, res) => {
-    // console.log('---------------------------------------------');
-    // console.log('on get /api/');
-    // console.log({req, res});
-    // console.log();
-    // console.log();
-    // console.log();
-    // console.log();
-    // console.log('---------------------------------------------');
+    console.log("server.get('/api')");
 
     res.json({msg: 'hello from express'});
+    res.end();
 
 });
 
-// interface chatsQuery extends QueryString.ParsedQs {
-//     chatId: number;
-// };
-
-server.get('/api/chats', (req, res) => {
-
-    // res.json({req, res});
-    // res.end(JSON.stringify({req, res}, null, 4));
-    // res.end(safeStringify({req, res}));
-    // res.end(inspect({req, res}) + '\n' + inspect({client: req.client}));
-
-
-    // const basicReq = (
-    //     ({app, baseUrl, body, cookies, fresh, hostname, ip, ips, method, originalUrl, params, path, protocol, query, route, secure, signedCookies, stale, subdomains, xhr}) =>
-    //     ({app, baseUrl, body, cookies, fresh, hostname, ip, ips, method, originalUrl, params, path, protocol, query, route, secure, signedCookies, stale, subdomains, xhr})
-    //  )(req);
-    // res.end(inspect(basicReq));
+server.get('/api/chats', async (req, res) => {
+    console.log("server.get('/api/chats')");
 
     const query = req.query;
 
     console.log({query});
-
-    // const chat = getChat(query.chatId)
-
-    res.end()
+    if (query.chatId && typeof query.chatId === 'string'){
+        const chat =  await getChat(parseInt(query.chatId));
+        res.end(inspect(chat));
+    } else {
+        const chats = await getAllChats({"chatId": true, "Questions": true, "Tags": true});
+        // res.end(inspect(chats));
+        res.end(inspect(chats?.map(c => { return {chatId: c.chatId, chat: c}; }), undefined, 4));
+    }
 
 
 });
 
+server.get(/.(css|js|jpg|jpeg|png|svg)$/, (req, res) => {
+    console.log('server.get(/.(css|js|jpg|jpeg|png|svg)$/');
+    
+    console.log({ __dirname, req_url: req.url});
+    // const filePath = __dirname + '/web/frontend/' + req.url;
+    const filePath = __dirname + '/web/frontend/dist/' + req.url;
+    res.sendFile(path.resolve(filePath), (error) => {
+
+        if (error){
+            console.error({ error });
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end(JSON.stringify({ error }));
+        } 
+    });
+});
 
 
 export default server.listen(PORT, HOST, ()=>{
     console.log('server: Running at '+HOST+':'+PORT);
 });
-
-
-/*
-export default http.createServer(function (request, response){
-
-    console.log('server: request starting for ');
-    console.log(request);
-
-    var filePath = '.' + request.url;
-    // if (filePath == './')
-        filePath = './index.html'
-
-    console.log(filePath);
-
-    const extname = path.extname(filePath);
-    let contentType: string;
-    
-    switch (extname){
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        default:
-            contentType = 'text/html';
-    }
-
-    fs.exists(filePath, function(exists){
-
-        if (exists){
-            fs.readFile(filePath, function (error, content) {
-                if (error) {
-                    response.writeHead(500);
-                    response.end();
-                } else {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                }
-            });
-        } 
-        else {
-            response.writeHead(404);
-            response.end();
-        }
-
-    });
-
-}).listen(PORT, HOST, () => {
-    console.log('server: Running at '+HOST+':'+PORT);
-});
-
-/**
- * 
- */
-
 
 

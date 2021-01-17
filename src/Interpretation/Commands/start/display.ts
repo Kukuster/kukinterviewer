f3533a049217ed4775f0b8adf41b22dc9c521034
Kukuster <KukusterMOP@gmail.com>
2021-01-17 19:15:36 +0200

    added a TODO, corrected/improved greeting message

--- src/Interpretation/Commands/start/display.ts
+++ src/Interpretation/Commands/start/display.ts
@@ -14,25 +14,27 @@ export default async function start_display(msg: IIMessage, data: start_execute_
         
         const firstReply = `Hi! Need help preparing for an interview?
 
-First things first, I'm not too wordy. That's mostly your chat, and it's your message history. Tell me what to do explicitly in simple English in the imperative form. Otherwise, I'll ignore the message as you may write what you want and answer your interview questions in whatever form you prefer to see in this chat history.
+First things first.
+ • That's mostly your chat, and it's your message history.
+ • Tell me what to do explicitly in simple English in the imperative form, a command per message. I'll ignore other messages, as you may write what you want and answer your interview questions in whatever form you prefer to see in this chat history.
 
-I store the data, so you can ask me to add a question to my permanent list. I help you to deal with tags in questions, so you can ask me to list tags, tag/untag questions.
+What can I do?
+ • I store the data, so you can ask me to add a question to my permanent list. I remember tags in your questions, so you can ask me to show questions by tags, tag/untag questions.
 
-I have a schedule, so you can tell me when and how often I should be asking you, and I'll be asking you random questions.
+ • I have a schedule, so you can tell me when and how often I should be asking you, and I'll be asking you random questions from your list.
 
-Whichever topic you want to prepare for, choose what questions or tags to disable accordingly, and I won't be asking you those. When asking a question, I disable it so I won't choose it again at random (unless you tell me so, of course).
+ • Whichever topic you want to prepare for, choose what questions or tags to disable accordingly, and I won't be asking you those. When asking a question, I disable it so I won't choose it again at random (unless you tell me so, of course).
 
-Hope I'm making sense! I'm a simple bot. You tell me what to do — I do, if I understand. If I don't — I ignore.
+I hope I'm making sense! I'm a simple bot. You tell me what to do — I do, if I understand. If I don't — I ignore.
 
 I won't bother you with a lot of text anymore.`;
 
 
 
         const secondReply = `
-...except. I need to know your timezone, so I shift my schedule in accord.
+...except, I need to know your timezone so that I can shift my schedule in accord.
 
-Where do you reside? I know all the countries and a lot of cities.
-`;    
+Where do you reside? I know all the countries and a lot of cities.`;    
 
         const firstReply_result = await sendMessageSafely(chatId, firstReply, {
             parse_mode: 'Markdown'

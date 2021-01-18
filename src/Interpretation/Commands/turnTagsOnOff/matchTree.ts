028adc6fa6e83b892df46252d575dc1690f6ac9a
Kukuster <KukusterMOP@gmail.com>
2021-01-18 16:39:36 +0200

    enriched 3 matchTrees, improved tests for other 2:
    • setAskingTime Command now understands:
     - «don't ask me earlier than 10 am»
    
    • turnTagsOnOff Command now understands:
     - «don't ask me #foo questions»
     - «don't ask #foo questions»
     - «ask me no #foo questions»
     - «disable #foo questions»
     - «you can ask me #foo questions»
     - «don't ask me questions tagged #foo»
    
    • turnQuestionsOnOff Command now understands:
     - «don't ask me questions #4, 6, 10»

--- src/Interpretation/Commands/turnTagsOnOff/matchTree.ts
+++ src/Interpretation/Commands/turnTagsOnOff/matchTree.ts
@@ -14,6 +14,7 @@ const off = /^off[?!.,;:]*$/i;
 
 
 const all = /^(all|each|every)[?!.,;:]*$/i;
+const any = /^(all|each|every|any)[?!.,;:]*$/i;
 const digit = /(#|№|@|n(um(ber)?)?)?(\d+)(st|nd|rd|th)?[?!.,;:]*/gi;
 
 const tagWord = /^(hash)?tags?[?!.,;:]*$/i;
@@ -28,8 +29,16 @@ const neg = /^(not?|without)[?!.,;:]*$/i;
 const dont = /^(do(es)?n'?t|haven'?t|hadn'?t|weren'?t|aren'?t|ain'?t|never)[?!.,;:]*$/i;
 
 
-const frb = /^((hash)?tag(ged|ging)|question(s|ed|ing)?|eras(e|ing)|remov(e|ing)|turn(ing)?|delet(e|ing|ed)|eliminat(e|ing|ed)|destro(y|ing|ed)|drop(ping|ped)?|wip(e|ing|ed)|withdraw(ing|ed)?|enabl(e|ing)|disabl(e|ing)|dismiss(ing)|add(ing)?|new|creat(e|ing|ed)|insert(ing|ed)?|submit(ing|ed)?|includ(e|ing|ed)?)[?!.,;:]*$/i;
+const frb                = /^((hash)?tag(ged|ging)|question(s|ed|ing)?|eras(e|ing)|remov(e|ing)|turn(ing)?|delet(e|ing|ed)|eliminat(e|ing|ed)|destro(y|ing|ed)|drop(ping|ped)?|wip(e|ing|ed)|withdraw(ing|ed)?|enabl(e|ing)|disabl(e|ing)|dismiss(ing)|add(ing)?|new|creat(e|ing|ed)|insert(ing|ed)?|submit(ing|ed)?|includ(e|ing|ed)?)[?!.,;:]*$/i;
+const frb_allowQuestions = /^((hash)?tag(ged|ging)|question(ed|ing)?|eras(e|ing)|remov(e|ing)|turn(ing)?|delet(e|ing|ed)|eliminat(e|ing|ed)|destro(y|ing|ed)|drop(ping|ped)?|wip(e|ing|ed)|withdraw(ing|ed)?|enabl(e|ing)|disabl(e|ing)|dismiss(ing)|add(ing)?|new|creat(e|ing|ed)|insert(ing|ed)?|submit(ing|ed)?|includ(e|ing|ed)?)[?!.,;:]*$/i;
 
+
+const questions = /^question(s)?[?!.,;:]*$/i;
+
+const ask = /^(ask(ing|ed)?|interview(ed|ing)?)[?!.,;:]*$/i;
+
+
+const make = /^(let|make|turn)[?!.,;:]*$/i;
 
 
 
@@ -41,11 +50,14 @@ const allOn  :shoot = { turn: 'on',  Tags: 'all' },
      someOff :shoot = { turn: 'off', Tags: 'some' };
 
 
-let root_children: nodeLike[];
+let    root_children: nodeLike[],
+    dontAsk_children: nodeLike[];
 
 
 export const turnTagsOnOff_tree =
 node(root, [
+
+
 
     ...(root_children = [
     node(turn, [
@@ -53,7 +65,7 @@ node(root, [
         // turn -> ...
         node(tag, [
             SELF,
-            
+
             // turn -> # -> ...
             node(tagWord, [
                 node(on, [
@@ -68,6 +80,20 @@ node(root, [
             ]),
 
             // turn -> # -> ...
+            node(questions, [
+                node(on, [
+                    node(off, []),
+                    node(frb, []),
+                ], someOn as shoot),
+                node(off, [
+                    node(on, []),
+                    node(frb, []),
+                ], someOff as shoot),
+                node(tagWord, []),
+                node(frb, []),
+            ]),
+
+            // turn -> # -> ...
             node(on, [
                 node(off, []),
                 node(frb, []),
@@ -76,8 +102,7 @@ node(root, [
                 node(on, []),
                 node(frb, []),
             ], someOff as shoot),
-            node(frb, []),
-            
+
             // turn -> # -> ...
             node(frb, []),
         ]),
@@ -108,7 +133,7 @@ node(root, [
             node(tag, [
                 SELF,
                 node(off, []),
-                node(frb, []),
+                node(frb_allowQuestions, []),
             ], someOn as shoot),
             node(all, [
                 node(tagWord, [
@@ -120,13 +145,13 @@ node(root, [
                 node(frb, []),
             ]),
             node(off, []),
-            node(frb, []),
+            node(frb_allowQuestions, []),
         ]),
         node(off, [
             node(tag, [
                 SELF,
                 node(on, []),
-                node(frb, []),
+                node(frb_allowQuestions, []),
             ], someOff as shoot),
             node(all, [
                 node(tagWord, [
@@ -138,7 +163,7 @@ node(root, [
                 node(frb, []),
             ]),
             node(on, []),
-            node(frb, []),
+            node(frb_allowQuestions, []),
         ]),
 
         // turn -> ...
@@ -158,14 +183,19 @@ node(root, [
         ]),
 
         // turn -> ...
-        node(frb, []),
+        node(frb_allowQuestions, []),
     ]),
+
+
+
+
+
 
     node(enable, [
         node(tag, [
             SELF,
             node(off, []),
-            node(frb, []),
+            node(frb_allowQuestions, []),
         ], someOn as shoot),
         // enable -> ...
         node(all, [
@@ -177,13 +207,13 @@ node(root, [
             node(frb, []),
         ]),
         node(off, []),
-        node(frb, []),
+        node(frb_allowQuestions, []),
     ]),
     node(disable, [
         node(tag, [
             SELF,
             node(on,  []),
-            node(frb, []),
+            node(frb_allowQuestions, []),
         ], someOff as shoot),
         // disable -> ...
         node(all, [
@@ -195,8 +225,12 @@ node(root, [
             node(frb, []),
         ]),
         node(on, []),
-        node(frb, []),
+        node(frb_allowQuestions, []),
     ]),
+
+
+
+
 
     node(tagWord, [
         // tag -> ...
@@ -251,7 +285,7 @@ node(root, [
             node(disable, [
                 node(frb, []),
             ], allOff as shoot),
-            
+
             // tag -> all -> ...
             node(neg, []),
             node(dont, []),
@@ -263,13 +297,229 @@ node(root, [
         node(dont, []),
         node(frb, []),
     ]), // tag
-    ]), // ...(root_children = […])
+    ]), // ...(root_children = […])  //
+
+
+
+
+
 
     node(neg, []),
+
+
+
+
+
     node(dont, [
+
+        // don't -> ...
         node(mind, root_children),
+
+        // don't -> ...
+        node(ask, dontAsk_children = [
+            node(tag, [
+                SELF,
+                node(on, []),
+                node(frb_allowQuestions, []),
+            ], someOff as shoot),
+
+            // don't -> ask -> ...
+            node(any, [
+                node(tagWord, [
+                    node(on, []),
+                    node(frb, []),
+                ], allOff as shoot),
+                node(on, []),
+                node(frb, []),
+            ]),
+            node(frb_allowQuestions, []),
+        ]),
+
+        // don't -> ...
         node(frb, []),
     ]),
+
+
+
+
+
+    node(ask, [
+        node(tag, [
+            SELF,
+            node(on, []),
+            node(frb_allowQuestions, []),
+        ], someOn as shoot),
+
+        // ask -> ...
+        node(any, [
+            node(tagWord, [
+                node(on, []),
+                node(frb, []),
+            ], allOn as shoot),
+            node(on, []),
+            node(frb, []),
+        ]),
+
+        node(neg, dontAsk_children),
+
+        node(frb_allowQuestions, []),
+    ]),
+
+
+
+
+
+    node(make, [
+
+        // make -> ...
+        node(tag, [
+            SELF,
+
+            // make -> # -> ...
+            node(tagWord, [
+                node(enable, [
+                    node(off, []),
+                    node(disable, []),
+                    node(frb, []),
+                ], someOn as shoot),
+                node(disable,[
+                    node(on, []),
+                    node(enable, []),
+                    node(frb, []),
+                ], someOff as shoot),
+                node(frb, []),
+            ]),
+
+            // make -> # -> ...
+            node(questions, [
+                node(enable, [
+                    node(off, []),
+                    node(disable, []),
+                    node(frb, []),
+                ], someOn as shoot),
+                node(disable, [
+                    node(on, []),
+                    node(enable, []),
+                    node(frb, []),
+                ], someOff as shoot),
+                node(tagWord, []),
+                node(frb, []),
+            ]),
+
+            // make -> # -> ...
+            node(enable, [
+                node(off, []),
+                node(disable, []),
+                node(frb, []),
+            ], someOn as shoot),
+            node(disable, [
+                node(on, []),
+                node(enable, []),
+                node(frb, []),
+            ], someOff as shoot),
+
+            // make -> # -> ...
+            node(frb_allowQuestions, []),
+        ]),
+
+        // make -> ...
+        node(tagWord, [
+            node(tag, [
+                SELF,
+
+                // make -> tag -> # -> ...
+                node(enable, [
+                    node(off, []),
+                    node(disable, []),
+                    node(frb, []),
+                ], someOn as shoot),
+                node(disable, [
+                    node(on, []),
+                    node(enable, []),
+                    node(frb, []),
+                ], someOff as shoot),
+                node(frb, []),
+
+                // make -> tag -> # -> ...
+                node(frb, []),
+            ]),
+        ]),
+
+        // make -> ...
+        node(enable, [
+            node(tag, [
+                SELF,
+                node(off, []),
+                node(disable, []),
+                node(frb_allowQuestions, []),
+            ], someOn as shoot),
+            node(all, [
+                node(tagWord, [
+                    SELF,
+                    node(off, []),
+                    node(disable, []),
+                    node(frb, []),
+                ], allOn as shoot),
+                node(off, []),
+                node(disable, []),
+                node(frb, []),
+            ]),
+            node(off, []),
+            node(disable, []),
+            node(frb_allowQuestions, []),
+        ]),
+        node(disable, [
+            node(tag, [
+                SELF,
+                node(on, []),
+                node(enable, []),
+                node(frb_allowQuestions, []),
+            ], someOff as shoot),
+            node(all, [
+                node(tagWord, [
+                    SELF,
+                    node(on, []),
+                    node(enable, []),
+                    node(frb, []),
+                ], allOff as shoot),
+                node(on, []),
+                node(enable, []),
+                node(frb, []),
+            ]),
+            node(on, []),
+            node(enable, []),
+            node(frb_allowQuestions, []),
+        ]),
+
+        // make -> ...
+        node(all, [
+            node(tagWord, [
+                node(enable, [
+                    node(off, []),
+                    node(disable, []),
+                    node(frb, []),
+                ], allOn  as shoot),
+                node(disable, [
+                    node(on, []),
+                    node(enable, []),
+                    node(frb, []),
+                ], allOff as shoot),
+                node(frb, []),
+            ]),
+            node(frb, []),
+        ]),
+
+        // make -> ...
+        node(frb_allowQuestions, []),
+
+
+    ]),
+
+
+
+
     node(frb, []),
+
+
 
 ]);
